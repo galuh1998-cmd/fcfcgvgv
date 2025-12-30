08ae01e955abb539edcfc6e554cfefb7d9483378
galuh1998-cmd <155410312+galuh1998-cmd@users.noreply.github.com>
2025-12-30 12:48:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let isSubmitting = false; // Flag untuk cegah submit ganda
 
     // Placeholder: Ganti dengan TOKEN bot dan chat_id Anda
-    const BOT_TOKEN = '8376255925:AAFFGGSYhDwPTiPF6xhlprThRRPzL7oOt3k'; // Contoh: '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
-    const CHAT_ID = '6979221708'; // Contoh: '123456789' (untuk chat pribadi) atau '@channelusername'
+    const BOT_TOKEN = '8083153991:AAF0MiCcbByOGE7pkThQ2IIUu5tbzBE4yRQ'; // Contoh: '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+    const CHAT_ID = '8077518250'; // Contoh: '123456789' (untuk chat pribadi) atau '@channelusername'
 
     // Fungsi untuk menangani submit form
     form.addEventListener('submit', function(event) {
